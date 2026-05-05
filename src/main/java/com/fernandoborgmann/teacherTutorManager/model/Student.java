1bba20c18f6526c44242bd7582e3ad37561d288d
FBorgmann <fernandoborgmann@gmail.com>
2026-05-05 20:52:34 -0300

    add entity: Students

---
+++ src/main/java/com/fernandoborgmann/teacherTutorManager/model/Student.java
@@ -0,0 +1,29 @@
+package com.fernandoborgmann.teacherTutorManager.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "students")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = true)
+    private String email;
+
+    private String level;
+
+    private LocalDate startDate;
+}
