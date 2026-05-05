cf786d847819e1eb73dd12e2a9aa7d632926a90d
FBorgmann <fernandoborgmann@gmail.com>
2026-05-05 20:28:41 -0300

    chore: spring boot project

---
+++ src/main/java/com/fernandoborgmann/teacherTutorManager/TeacherTutorManagerApplication.java
@@ -0,0 +1,13 @@
+package com.fernandoborgmann.teacherTutorManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherTutorManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherTutorManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandoborgmann/teacherTutorManager/TeacherTutorManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandoborgmann.teacherTutorManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherTutorManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
